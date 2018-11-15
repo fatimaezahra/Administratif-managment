8f8f1ebd552f13453c7835d63a1b808087c83871
soumaya moukhlis <soumaya.moukhlis@gmail.com>
2018-11-15 09:38:50 +0100

    init project

---
+++ insuranceProject/urls.py
@@ -0,0 +1,13 @@
+# djauth/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('insurance/',include('insurance.urls')),
+    # path('insurance/',TemplateView.as_view(template_name='employee_form.html'),name='create_emp'),
+]
